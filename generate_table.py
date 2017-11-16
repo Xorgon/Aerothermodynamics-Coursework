40c0c05f07f7a18260de20285fe63f8a185c19e1
Xorgon <norman.reggox@gmail.com>
2017-11-16 18:45:47 +0000

    Added Report LaTeX files.

--- generate_table.py
+++ generate_table.py
@@ -10,7 +10,7 @@ import numpy as np
 
 
 def mach_angle(M, degrees=True):
-    rads = math.asin(1./M)
+    rads = math.asin(1. / M)
     if degrees:
         return math.degrees(rads)
     else:
@@ -19,8 +19,8 @@ def mach_angle(M, degrees=True):
 
 def prandtl_meyer(M, gamma, degrees=True):
     g = (gamma + 1.) / (gamma - 1.)
-    rads = math.sqrt(g) * math.atan(math.sqrt((M*M - 1.) / g)) \
-        - math.atan(math.sqrt(M*M - 1.))
+    rads = math.sqrt(g) * math.atan(math.sqrt((M * M - 1.) / g)) \
+           - math.atan(math.sqrt(M * M - 1.))
     if degrees:
         return math.degrees(rads)
     else:
@@ -29,7 +29,7 @@ def prandtl_meyer(M, gamma, degrees=True):
 
 def generate_ift(M_min, M_max, M_step, gamma, degrees=True):
     table = []
-    for M in np.arange(M_min, M_max+1e-10, M_step):
+    for M in np.arange(M_min, M_max + 1e-10, M_step):
         table.append([round(M, 4), round(mach_angle(M), 2),
                       round(prandtl_meyer(M, gamma), 2)])
     return table
@@ -38,7 +38,7 @@ def generate_ift(M_min, M_max, M_step, gamma, degrees=True):
 def array_to_csv(array, filename, header=None):
     f = open(filename + ".csv", 'w')
     if header is not None:
-        f.write(header)
+        f.write(header + "\n")
     for row in array:
         line = ""
         for column in row:
@@ -46,3 +46,6 @@ def array_to_csv(array, filename, header=None):
             line += "," if column != row[-1] else ""
         f.write(line + "\n")
     f.close()
+
+
+array_to_csv(generate_ift(2.00, 2.20, 0.01, 5 / 3), "Report/argon_ift")
