c50c3e05aca4c8b0a608e2d2ce9c848ae9664396
Xorgon <norman.reggox@gmail.com>
2017-11-20 04:14:24 +0000

    Improved graph and analysis output.

--- examples/general_min_length.py
+++ examples/general_min_length.py
@@ -13,8 +13,9 @@ decimal = theta_max % 1
 integer = theta_max - decimal
 
 # init_thetas = [decimal, integer / 3 + decimal, 2 * integer / 3 + decimal, theta_max]
-
 init_thetas = np.logspace(np.log10(1e-2), np.log10(theta_max), 100, base=10)
+# init_thetas = np.linspace(1e-2, theta_max, 100)
+
 np.append(init_thetas, theta_max)
 
 ift = IFT(1, 3, 0.01, gamma)
@@ -23,13 +24,17 @@ init_points = []
 for theta in init_thetas:
     init_points.append(Point(str(theta), ift, gamma, theta=theta, x=0, y=1, set_all=False))
 
+internal_points = []
 wall_points = [init_points[-1]]
+symmetry_points = []
 for i in range(len(init_points)):
     init_p = init_points[i]
     init_p.set_M()
     last_p = Point(init_p.pid + "-sym", ift, gamma, b=init_p, y=0)
+    symmetry_points.append(last_p)
     for j in range(i + 1, len(init_points)):
         last_p = Point(init_p.pid + "-" + init_points[j].pid, ift, gamma, a=last_p, b=init_points[j])
+        internal_points.append(last_p)
     wall_points.append(Point(init_p.pid + "-wall", ift, gamma, b=wall_points[-1], a=last_p, wall=True))
 
 xs = []
@@ -40,21 +45,31 @@ for p in wall_points:
     ys.append(p.y)
     area_ratios.append(area_ratio(p.M, gamma))
 
+all_p_xs = []
+all_p_ys = []
+for p in init_points + symmetry_points + internal_points + wall_points:
+    all_p_xs.append(p.x)
+    all_p_ys.append(p.y)
+
 flat = []
 for x in xs:
     flat.append(0)
-
-# plt.plot(xs, ys, xs, flat, 'r--')
-# plt.show()
 
 fig = plt.figure()
 fig.patch.set_facecolor('white')
 ax = fig.gca()
 ax.plot(xs, ys)
-# ax.plot(xs, area_ratios)
+ax.plot(xs[-1], area_ratios[-1], 'ro')
+# ax.plot(all_p_xs, all_p_ys, 'gx')  # Show all points.
+ax.annotate("$A/A^{*}$", (xs[-1], area_ratios[-1]), (xs[-1] - 0.6, area_ratios[-1] - 0.2),
+            arrowprops={"arrowstyle": "->"})
 ax.plot(xs, flat, 'r--')  # Change to 'ro' to show point distribution.
 ax.set_xlabel(r'$x/r_{a}$', )
 ax.set_ylabel(r'$y/r_{a}$')
-ax.axis([0, xs[-1], -0.1, ys[-1] + 0.1])
+ax.axis([0, xs[-1] + 0.1, -0.1, ys[-1] + 0.1])
 # plt.legend([explicit_avgs_plot, implicit_avgs_plot], ['Explicit', 'Implicit'], loc=4)
 plt.show()
+
+print("Area ratio at final point = {0:.3f}".format(area_ratios[-1]))
+print("Final point x coordinate = {0:.3f}".format(xs[-1]))
+print("Final point y coordinate = {0:.3f}".format(ys[-1]))
